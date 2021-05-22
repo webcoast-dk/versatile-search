bcbf2f865af71a2a3bf60362237017ae632d291a
Thorben Nissen <thorben@webcoast.dk>
2021-05-22 21:43:24 +0200

    [RELEASE] 1.0.1
    ## Bugfix
    Wrong default value for `settings.pagination.itemsPerPage`

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Versatile Search',
     'description' => 'A versatile and extendable search extension',
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'category' => 'plugin',
     'constraints' => [
         'depends' => [
