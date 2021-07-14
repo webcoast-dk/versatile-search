1520faae0f6ce667a20382afdd6d85a1ad3efdd6
Thorben Nissen <thorben@webcoast.dk>
2021-07-14 15:41:48 +0200

    [BUGFIX] Move plugin registration to TCA overrides

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,7 +1,10 @@
 <?php
 
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['versatilesearch_search'] = 'pages,recursive';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['versatilesearch_search'] = 'pi_flexform';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['versatilesearch_form'] = 'pages,recursive';
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('versatilesearch_search', 'FILE:EXT:versatile_search/Configuration/FlexForm/search.xml');
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['versatilesearch_search'] = 'pi_flexform';
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('versatilesearch_search', 'FILE:EXT:versatile_search/Configuration/FlexForm/search.xml');
+
+TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('WEBcoast.VersatileSearch', 'Search', 'LLL:EXT:versatile_search/Resources/Private/Language/backend.xlf:plugin.search.title');
+TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('WEBcoast.VersatileSearch', 'Form', 'LLL:EXT:versatile_search/Resources/Private/Language/backend.xlf:plugin.form.title');
