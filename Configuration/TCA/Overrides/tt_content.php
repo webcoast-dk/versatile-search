14483a59955abf3c304623ee23b88546c2b27d31
Thorben Nissen <thorben@webcoast.dk>
2022-10-07 11:47:22 +0200

    [FEATURE] Register as content elements
    * change registration from plugin to content element
    * configure types
    * add content element wizard configuration

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,10 +1,12 @@
 <?php
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['versatilesearch_search'] = 'pages,recursive';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['versatilesearch_form'] = 'pages,recursive';
+TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('WEBcoast.VersatileSearch', 'Search', 'LLL:EXT:versatile_search/Resources/Private/Language/backend.xlf:plugin.search.title', 'EXT:versatile_search/Resources/Public/Icons/Extension.svg', 'forms');
+TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('WEBcoast.VersatileSearch', 'Form', 'LLL:EXT:versatile_search/Resources/Private/Language/backend.xlf:plugin.form.title', 'EXT:versatile_search/Resources/Public/Icons/Extension.svg', 'forms');
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['versatilesearch_search'] = 'pi_flexform';
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('versatilesearch_search', 'FILE:EXT:versatile_search/Configuration/FlexForm/search.xml');
+$GLOBALS['TCA']['tt_content']['types']['versatilesearch_search'] = $GLOBALS['TCA']['tt_content']['types']['list'];
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', 'pi_flexform', 'versatilesearch_search', 'after:list_type');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('', 'FILE:EXT:versatile_search/Configuration/FlexForm/search.xml', 'versatilesearch_search');
+$GLOBALS['TCA']['tt_content']['types']['versatilesearch_search']['showitem'] = preg_replace('/(?<=,)\s*(list_type|pages|recursive).*?,/', '', $GLOBALS['TCA']['tt_content']['types']['versatilesearch_search']['showitem']);
 
-TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('WEBcoast.VersatileSearch', 'Search', 'LLL:EXT:versatile_search/Resources/Private/Language/backend.xlf:plugin.search.title');
-TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('WEBcoast.VersatileSearch', 'Form', 'LLL:EXT:versatile_search/Resources/Private/Language/backend.xlf:plugin.form.title');
+$GLOBALS['TCA']['tt_content']['types']['versatilesearch_form'] = $GLOBALS['TCA']['tt_content']['types']['list'];
+$GLOBALS['TCA']['tt_content']['types']['versatilesearch_form']['showitem'] = preg_replace('/(?<=,)\s*(list_type|pages|recursive).*?,/', '', $GLOBALS['TCA']['tt_content']['types']['versatilesearch_form']['showitem']);
