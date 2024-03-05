f35417e85322d4404593c7af741804ec3d0246bd
Thorben Nissen <thorben@webcoast.dk>
2024-03-05 11:37:02 +0100

    [BUGFIX] Do not translate TypoScript include

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,4 +3,5 @@
 TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     'versatile_search',
     'Configuration/TypoScript/',
-    TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(TYPO3\CMS\Core\Localization\LanguageService::class)->sL('LLL:EXT:versatile_search/Resources/Private/Language/backend.xlf:plugin.typoscript'));
+    'Basic search settings'
+);
